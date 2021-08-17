e445f9ca472885bc8800b2dd0d06b11354ea3baa
Michael Bruty <michael.bruty@students.plymouth.ac.uk>
2021-08-17 22:36:44 +0100

    Configure font awesome icons

--- web-client/src/main.ts
+++ web-client/src/main.ts
@@ -1,9 +1,15 @@
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faSearch, faChevronRight, faExclamationTriangle } from '@fortawesome/free-solid-svg-icons';
 import { createApp } from 'vue';
-import vuetify from './plugins/vuetify';
-import App from './App.vue';
 import router from './router';
+import App from './App.vue';
+
+library.add(faSearch);
+library.add(faChevronRight);
+library.add(faExclamationTriangle);
 
 createApp(App)
   .use(router)
-  .use(vuetify)
+  .component('font-awesome-icon', FontAwesomeIcon)
   .mount('#app');
